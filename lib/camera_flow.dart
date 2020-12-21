ab0409d632ffb5be67d0220bbb3d236f71965e03
Ameer Hamza Tariq <30323486+ahamzatariq@users.noreply.github.com>
2020-12-21 17:17:14 +0500

    Create the Camera/Gallery Flow

---
+++ lib/camera_flow.dart
@@ -0,0 +1,63 @@
+import 'package:aws/pages/gallery_page.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/camera_page.dart';
+
+class CameraFlow extends StatefulWidget {
+  final VoidCallback shouldLogOut;
+
+  CameraFlow({Key key, this.shouldLogOut}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CameraFlowState();
+}
+
+class _CameraFlowState extends State<CameraFlow> {
+  CameraDescription _camera;
+
+  bool _shouldShowCamera = false;
+
+  List<MaterialPage> get _pages {
+    return [
+      MaterialPage(child: Placeholder()),
+      if (_shouldShowCamera)
+        MaterialPage(
+          child: CameraPage(
+            camera: _camera,
+            didProvideImagePath: (imagePath) {
+              this._toggleCameraOpen(false);
+            },
+          ),
+        )
+    ];
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Navigator(
+      pages: _pages,
+      onPopPage: (route, result) => route.didPop(result),
+    );
+  }
+
+  void _toggleCameraOpen(bool isOpen) {
+    setState(() {
+      this._shouldShowCamera = isOpen;
+    });
+  }
+
+  void _getCamera() async {
+    final camerasList = await availableCameras();
+    setState(() {
+      final firstCamera = camerasList.first;
+      this._camera = firstCamera;
+    });
+  }
+}
